eb8e3361e116d04904da2f7442e584d5fcaf0e8f
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2019-09-28 15:08:28 +0300

    Added first version of master slave node discovery

---
+++ src/main/kotlin/bigd/Config.kt
@@ -0,0 +1,5 @@
+package bigd
+
+const val MASTER_HOST = "localhost"
+const val MASTER_PORT = 50051
+val SLAVE_PORT_RANGE: ArrayList<Int> = arrayListOf(50052, 50060)
---
+++ src/main/kotlin/bigd/Slave.kt
@@ -0,0 +1,87 @@
+package bigd
+
+import bigd.grpc.*
+import io.grpc.*
+import io.grpc.stub.StreamObserver
+import java.util.*
+import java.util.concurrent.TimeUnit
+import java.util.logging.Level
+import java.util.logging.Logger
+
+private class MapReduce: MapReducerGrpc.MapReducerImplBase() {
+    val logger = Logger.getLogger(MapReduce::class.java.name)
+
+    override fun mapReduce(request: MapReduceRequest, responseObserver: StreamObserver<MapReduceReply>) {
+        logger.info("received request for file URI ${request.uri}")
+        val reply = MapReduceReply.newBuilder().setMessage("Hello " + request.uri).build()
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
+
+class Slave(private val nodeId: String, host: String, port: Int) {
+    private val channel: ManagedChannel = ManagedChannelBuilder.forAddress(host, port)
+            .usePlaintext()
+            .build()
+    private val blockingStub: GreeterGrpc.GreeterBlockingStub
+    private val logger = Logger.getLogger(Slave::class.java.name)
+    private var server: Server? = null
+
+    init {
+        this.blockingStub = GreeterGrpc.newBlockingStub(channel)
+    }
+
+    fun greet(): GreetingReply? {
+        logger.info("Will try to greet master at $MASTER_HOST:$MASTER_PORT")
+        val request = GreetingRequest.newBuilder().setNodeId(nodeId).build()
+        val response: GreetingReply
+        try {
+            response = blockingStub.greetMaster(request)
+        } catch (e: StatusRuntimeException) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.status)
+            return null
+        }
+
+        logger.info("Master is saying: " + response.message)
+        return response
+    }
+
+    fun startServer(port: Int) {
+        val mapReduce = MapReduce()
+        server = ServerBuilder.forPort(port)
+                .addService(MapReduce())
+                .build()
+                .start()
+        mapReduce.logger.info("Server started, listening on $MASTER_PORT")
+
+        Runtime.getRuntime().addShutdownHook(object : Thread() {
+            override fun run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down")
+                server?.shutdown()
+                shutdownChannel()
+                System.err.println("*** server shut down")
+            }
+        })
+    }
+
+    fun shutdownChannel() {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    fun blockUntilShutdown() {
+        server?.awaitTermination()
+    }
+}
+
+fun main() {
+    val nodeId = UUID.randomUUID().toString()
+    val slave = Slave(nodeId, MASTER_HOST, MASTER_PORT)
+    val reply = slave.greet()
+    if (reply == null) {
+        slave.shutdownChannel()
+        return
+    }
+    slave.startServer(reply.port)
+    slave.blockUntilShutdown()
+}
